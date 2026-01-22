8b73201232be82c4b9fe9254ee8342e24c630e6c
Zequi <ezequielcarracedoignacio@gmail.com>
2026-01-22 20:52:16 +0100

    commit grabar ficheros

--- src/main/java/app/util/gestorFicheros.java
+++ src/main/java/app/util/gestorFicheros.java
@@ -1,15 +1,37 @@
 package main.java.app.util;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import main.java.app.modelo.Incidencia;
+import main.java.app.modelo.Usuario;
+
 public class gestorFicheros {
+    utils utilitats = new utils();
 
-    public void grabarCliente(){
+    public void grabarClientes(ArrayList<Incidencia> llistatIncidencies, File ficheroClientes) throws IOException {
+        BufferedReader buffer = utilitats.obrirLectors(ficheroClientes);
+        BufferedWriter escriptor = utilitats.obrirEscriptors(ficheroClientes);
+
+        for (int it = 0; it < llistatIncidencies.size(); it++) {
+            escriptor.write(llistatIncidencies.get(it).getId());
+            escriptor.write(llistatIncidencies.get(it).getDescripcion());
+            escriptor.write(String.valueOf(llistatIncidencies.get(it).getEstado()));
+            escriptor.write(llistatIncidencies.get(it).getUser().id());
+            escriptor.write(llistatIncidencies.get(it).getUser().nom());
+            escriptor.write(llistatIncidencies.get(it).getUser().email());
+            escriptor.flush();
+        }
 
     }
 
-    public void cargarFichero(){
+    public void cargarFichero() {
 
     }
 
-
-    
 }
--- src/main/java/app/util/utils.java
+++ src/main/java/app/util/utils.java
@@ -1,9 +1,18 @@
 package main.java.app.util;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class utils {
     Scanner scan = new Scanner(System.in);
+
+  
 
     public String demanarString(String missatge){ 
         System.out.println(missatge);
@@ -11,5 +20,21 @@ public class utils {
 
         return dada;
     }
+
+
+    public BufferedReader obrirLectors(File ficheroClientes)throws FileNotFoundException{
+        FileReader lector = new FileReader(ficheroClientes);
+
+        return new BufferedReader(lector);
+    }
+
+    public BufferedWriter obrirEscriptors(File ficheroClientes) throws IOException{
+        FileWriter escriptor = new FileWriter(ficheroClientes);
+
+        return new BufferedWriter(escriptor);
+    }
+
+
+
 
 }
